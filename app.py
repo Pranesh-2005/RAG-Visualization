c93d60bd6322420c1d5cd19bcfb26ece5f8035ac
Pranesh <praneshmadhan646@gmail.com>
2026-01-25 19:54:07 +0530

    init: added embedding model, read pdf and chunk text function

--- app.py
+++ app.py
@@ -8,3 +8,19 @@ import PyPDF2
 
 from sentence_transformers import SentenceTransformer
 from sklearn.decomposition import PCA
+
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+def read_pdf(file):
+    reader = PyPDF2.PdfReader(file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
+    return text.strip()
+
+def chunk_text(text, chunk_size):
+    words = text.split()
+    return [
+        " ".join(words[i:i + chunk_size])
+        for i in range(0, len(words), chunk_size)
+    ]
